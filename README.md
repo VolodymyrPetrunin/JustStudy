9303a43daa24ed66ddf3d48a4ac21727d47f4db2
Volodymyr Petrunin <vovachegotebenado@gmail.com>
2023-01-02 14:13:04 +0100

    Added README file

---
+++ README.md
@@ -0,0 +1,5 @@
+# This is test project
+## For java 
+
+- I am study Git.
+- Now i know how it work.
--- src/itProger/Main.java
+++ src/itProger/Main.java
@@ -1,6 +1,6 @@
 package itProger;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        System.out.println("git add ." + "git branch test" + "git checkout test" + "");
     }
 }
